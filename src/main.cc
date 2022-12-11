387f0e55fe7db867c58ffe19729d5a3bf71b1178
ARI Montecatini Terme <105586774+arimontecatiniterme@users.noreply.github.com>
2022-12-11 14:56:17 +0100

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -2,6 +2,8 @@
 
 #define LED_BUILTIN 2
 
+int rit=1000; // indica i ms di ritardo
+int var=0;    // una variabile di prova
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -11,10 +13,13 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+  var++; // incrementa la variabile di prova
+  
+  
   digitalWrite(LED_BUILTIN, HIGH);   // Arduino: turn the LED on (HIGH)
                                      // D1 Mini: turns the LED *off*
-  delay(1000);                       // wait for a second
+  delay(rit);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // Arduino: turn the LED off (LOW)
                                      // D1 Mini: turns the LED *on*
-  delay(1000);                       // wait for a second
+  delay(rit);                       // wait for a second
 }
